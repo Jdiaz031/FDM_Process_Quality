53597e34a4e80bd2d91c61ef2357210253b11c55
Jdiaz031 <111522942+Jdiaz031@users.noreply.github.com>
2024-04-05 14:27:20 -0600

    Add files via upload

--- raspberrypi_setup.sh
+++ raspberrypi_setup.sh
@@ -4,7 +4,4 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo raspi-config
 sudo apt-get install python3-dev python3-pip python3-smbus i2c-tools git -y
-sudo pip3 install -r requirements.txt
-git clone https://github.com/mccdaq/daqhats.git
-cd ./daqhats/
-sudo ./install.sh
+sudo pip install -r requirements.txt
