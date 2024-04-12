59b876144a5ac9ef902246eebfb1124ab9029d5b
Jdiaz031 <111522942+Jdiaz031@users.noreply.github.com>
2024-04-12 15:38:10 -0600

    Update raspberrypi_setup.sh

--- raspberrypi_setup.sh
+++ raspberrypi_setup.sh
@@ -3,5 +3,8 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo raspi-config
-sudo apt-get install python3-dev python3-pip python3-smbus i2c-tools git -y
-sudo pip install -r requirements.txt
+sudo apt-get install python3-dev python3-pip python3-smbus i2c-tools git python3-multiprocess libraspberrypi-dev -y
+git clone https://github.com/mccdaq/daqhats
+cd daqhats
+sudo ./install.sh
+sudo daqhats_read_eeproms
